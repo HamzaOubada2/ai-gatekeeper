6749d29dcee0c0c0d88a77ad672d097f314881fe
HamzaOubada2 <oubham220@gmail.com>
2026-08-15 00:07:56 +0100

    feat: migrate from Express to Fastify for improved performance
    - Added @nestjs/platform-fastify and fastify dependencies to package.json.
    - Updated main.ts to create a NestJS application using Fastify as the HTTP server.
    - Adjusted server listening configuration to log the running server URL.
    - Minor formatting changes in why.md for consistency.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, // use Fastify as HTTP server instead Express
+   NestFastifyApplication  //This tells TypeScript that the app is a NestJS application built using Fastify.
+} from '@nestjs/platform-fastify';
 
+  
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const  app = await NestFactory.create<NestFastifyApplication>(
+    AppModule, 
+    new FastifyAdapter(), //Use Fastify as your HTTP platform.
+  );
+
+
+  await app.listen(3000, "0.0.0.0");
+  console.log("Server Is Running on: http://localhost:3000")
 }
 bootstrap();
